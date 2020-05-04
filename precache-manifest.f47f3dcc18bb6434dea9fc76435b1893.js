cd496304127e9fc9a447ed33725f52fe1c37be97
upendarsankoju7 <50440972+upendarsankoju7@users.noreply.github.com>
2020-05-04 16:01:14 +0530

    initial commit

---
+++ precache-manifest.f47f3dcc18bb6434dea9fc76435b1893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6024afe98878ec7a85b2c6e63412144f",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "13fbb58b8609d708613a",
+    "url": "/notes/static/css/main.e23dfaed.chunk.css"
+  },
+  {
+    "revision": "dadaaa826cd744022809",
+    "url": "/notes/static/js/2.1ff0b93e.chunk.js"
+  },
+  {
+    "revision": "7bdab496cd9f260b01125fbfa557a79e",
+    "url": "/notes/static/js/2.1ff0b93e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13fbb58b8609d708613a",
+    "url": "/notes/static/js/main.cb2faddd.chunk.js"
+  },
+  {
+    "revision": "6a89469c1c2fa82483ce",
+    "url": "/notes/static/js/runtime-main.917b9e8e.js"
+  }
+]);
